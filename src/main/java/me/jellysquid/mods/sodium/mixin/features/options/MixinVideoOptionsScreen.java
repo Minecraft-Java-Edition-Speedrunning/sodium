d7bac7eaf4bd827ffcb44a66323f5e76f7f29212
tildejustin <spacepepper3.14@gmail.com>
2025-07-11 19:45:38 -0400

    reset gamma option properties on screen close

--- src/main/java/me/jellysquid/mods/sodium/mixin/features/options/MixinVideoOptionsScreen.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/options/MixinVideoOptionsScreen.java
@@ -15,6 +15,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.ArrayList;
@@ -28,11 +29,11 @@ public class MixinVideoOptionsScreen extends GameOptionsScreen {
         super(parent, gameOptions, title);
     }
 
-    @Redirect(method = "init", at=@At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/ButtonListWidget;addAll([Lnet/minecraft/client/options/Option;)V"))
+    @Redirect(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/ButtonListWidget;addAll([Lnet/minecraft/client/options/Option;)V"))
     private void optionsSwap(ButtonListWidget list, Option[] old_options) {
         Option.GAMMA.setMax(this.client.world == null ? 5 : 1);
         ((DoubleOptionAccessor) Option.GAMMA).setStep(this.client.world == null ? 0.01f : 0); // 0.01f step ensures we get an exact .2f so "Bright" shows up even when slider goes to 500
-        List<Option> options =  new ArrayList<>(Arrays.asList(old_options));
+        List<Option> options = new ArrayList<>(Arrays.asList(old_options));
         SodiumGameOptions.SpeedrunSettings speedrunSettings = SodiumClientMod.options().speedrun;
         if (speedrunSettings.showEntityCulling) {
             options.add(VanillaOptions.ENTITY_CULLING);
@@ -41,6 +42,12 @@ public class MixinVideoOptionsScreen extends GameOptionsScreen {
             options.add(VanillaOptions.FOG_OCCLUSION);
         }
         list.addAll(options.toArray(new Option[0]));
+    }
+
+    @Inject(method = "removed", at = @At("HEAD"))
+    private void resetGammaOptionProperties(CallbackInfo ci) {
+        Option.GAMMA.setMax(1);
+        ((DoubleOptionAccessor) Option.GAMMA).setStep(0);
     }
 
     @Inject(method = "mouseReleased", at = @At("RETURN"))
